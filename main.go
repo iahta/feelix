d24e60dddacb3356ca621dc1d011765fd802f566
iahta <153889016+iahta@users.noreply.github.com>
2025-05-05 16:42:05 -0500

    create database

--- main.go
+++ main.go
@@ -1,27 +1,53 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/iahta/feelix/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	database *database.Queries
+}
 
 func main() {
 	if err := godotenv.Load(); err != nil && !os.IsNotExist(err) {
 		log.Fatal("error loading env")
 	}
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("error loading database")
+	}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Unable to call database: %v", err)
+	}
+
+	dbQueries := database.New(db)
+	ok := []byte("OK")
+	apiCfg := apiConfig{
+		database: dbQueries,
+	}
 
 	mux := http.NewServeMux()
-	appHandler := http.StripPrefix("/", http.FileServer(http.Dir("./frontend")))
-	mux.Handle("/", appHandler)
+	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir("./frontend")))
+	mux.Handle("/app/", appHandler)
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(ok)
+	})
 
 	server := &http.Server{
 		Addr:         ":" + port,
---
+++ sql/queries/users.sql
@@ -0,0 +1,27 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, email, password_hash)
+VALUES (
+    gen_random_uuid(),
+    NOW(),
+    NOW(),
+    $1,
+    $2
+)
+RETURNING *;
+
+-- name: DeleteUsers :exec
+DELETE FROM users;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users
+WHERE email = $1;
+
+-- name: GetUserById :one
+SELECT * FROM users
+WHERE id = $1;
+
+-- name: UpdatePasswordEmailUser :one
+UPDATE users
+SET email = $1, password_hash = $2, updated_at = $3
+WHERE id = $4
+RETURNING id, created_at, updated_at, email;
